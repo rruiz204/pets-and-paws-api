fbcd04ef34835982910382b4e34a9379a496f5a0
roney ruiz <roneyruizrojas@gmail.com>
2024-08-23 10:42:29 -0600

    implement new components

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using Pets_And_Paws_Api.App.Database;
 using Pets_And_Paws_Api.App.Repositories;
 using Pets_And_Paws_Api.App.Repositories.Interfaces;
+using Pets_And_Paws_Api.App.Services;
+using Pets_And_Paws_Api.App.Services.Interfaces;
+using Pets_And_Paws_Api.App.Utilities;
+using Pets_And_Paws_Api.App.Utilities.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +21,19 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DatabaseContext>(opts => 
     opts.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
 
+// Services
+builder.Services.AddScoped<IAuthService, AuthService>();
+
 // Repositories
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IResetTokenRepository, ResetTokenRepository>();
+
+// Utilities
+builder.Services.AddScoped<IEncrypt, Encrypt>();
+builder.Services.AddScoped<ITokens, Tokens>();
+
+// Auto Mapper
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
