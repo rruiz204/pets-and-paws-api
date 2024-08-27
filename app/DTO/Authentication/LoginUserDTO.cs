f7c29b3f32b339753dd3929e6aee61b297501561
roney ruiz <roneyruizrojas@gmail.com>
2024-08-27 10:00:20 -0600

    commit deleted files

--- app/DTO/Authentication/LoginUserDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace Pets_And_Paws_Api.App.DTO.Authentication;
-
-public class LoginUserDTO
-{
-  public string Email { get; set; }
-  public string Password { get; set; }
-}
--- app/Utilities/Interfaces/IEncrypt.cs
+++
@@ -1,7 +0,0 @@
-namespace Pets_And_Paws_Api.App.Utilities.Interfaces;
-
-public interface IEncrypt
-{
-  string Hash(string password);
-  bool Verify(string hash, string password);
-}
