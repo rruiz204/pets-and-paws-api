743e22ba469df57e649be0e1233f6f45bf158bd1
roney ruiz <roneyruizrojas@gmail.com>
2024-08-28 11:00:18 -0600

    implement base repositoy in the user repository

--- App/Domain/Repositories/IUserRepository.cs
+++ App/Domain/Repositories/IUserRepository.cs
@@ -2,11 +2,7 @@ using Pets_And_Paws_Api.App.Domain.Models;
 
 namespace Pets_And_Paws_Api.App.Domain.Repositories;
 
-public interface IUserRepository
+public interface IUserRepository : IBaseRepository<User>
 {
-  Task<List<User>> ListUsers();
-  Task<User> GetUser(int id);
-  Task<User> CreateUser(User user);
-  Task<User> UpdateUser(User user);
-  Task<User?> FindUser(User user);
+  Task<User?> FindToValidRegister(User user);
 }
--- App/Infrastructure/Repositories/UserRepository.cs
+++ App/Infrastructure/Repositories/UserRepository.cs
@@ -1,42 +1,15 @@
+using System.Linq.Expressions;
 using Pets_And_Paws_Api.App.Domain.Models;
-using Microsoft.EntityFrameworkCore;
 using Pets_And_Paws_Api.App.Infrastructure.Database;
 using Pets_And_Paws_Api.App.Domain.Repositories;
 
 namespace Pets_And_Paws_Api.App.Infrastructure.Repositories;
 
-public class UserRepository(DatabaseContext context) : IUserRepository
+public class UserRepository(DatabaseContext context) : BaseRepository<User>(context), IUserRepository
 {
-  private readonly DatabaseContext _context = context;
-
-  public async Task<List<User>> ListUsers()
+  public async Task<User?> FindToValidRegister(User user)
   {
-    return await _context.User.ToListAsync();
-  }
-
-  public async Task<User> GetUser(int id)
-  {
-    return await _context.User.FirstAsync(u => u.Id == id);
-  }
-
-  public async Task<User> CreateUser(User user)
-  {
-    await _context.User.AddAsync(user);
-    await _context.SaveChangesAsync();
-    return await GetUser(user.Id);
-  }
-
-  public async Task<User> UpdateUser(User user)
-  {
-    _context.User.Update(user);
-    await _context.SaveChangesAsync();
-    return await GetUser(user.Id);
-  }
-
-  public async Task<User?> FindUser(User user)
-  {
-    return await _context.User.FirstOrDefaultAsync(
-      u => u.Email == user.Email || u.FirstName == user.FirstName
-    );
+    Expression<Func<User, bool>> predicate = u => u.Email == user.Email || u.FirstName == user.FirstName;
+    return await FindAsync(predicate);
   }
 }
