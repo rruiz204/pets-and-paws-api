d2555251b0610ce795aa77f4d2ec1d8968ee6732
roney ruiz <roneyruizrojas@gmail.com>
2024-09-04 11:47:04 -0600

    modify role repository dependencies

--- App/Domain/Repositories/IRoleRepository.cs
+++ App/Domain/Repositories/IRoleRepository.cs
@@ -2,7 +2,7 @@ using Pets_And_Paws_Api.App.Domain.Models;
 
 namespace Pets_And_Paws_Api.App.Domain.Repositories;
 
-public interface IRoleRepository : IBaseRepository<Role>
+public interface IRoleRepository
 {
-  
+  Task<List<Role>> GetAllAsync();
 }
--- App/Infrastructure/Repositories/RoleRepository.cs
+++ App/Infrastructure/Repositories/RoleRepository.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Pets_And_Paws_Api.App.Domain.Models;
 using Pets_And_Paws_Api.App.Domain.Repositories;
 using Pets_And_Paws_Api.App.Infrastructure.Database;
 
 namespace Pets_And_Paws_Api.App.Infrastructure.Repositories;
 
-public class RoleRepository(DatabaseContext context) 
-  : BaseRepository<Role>(context), IRoleRepository
+public class RoleRepository(DatabaseContext context) : IRoleRepository
 {
+  private readonly DatabaseContext _context = context;
 
+  public async Task<List<Role>> GetAllAsync()
+  {
+    return await _context.Role.ToListAsync();
+  }
 }
