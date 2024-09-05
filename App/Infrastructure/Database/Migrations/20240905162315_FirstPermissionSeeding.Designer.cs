ccb237f0fd0f53e153b356887e8d2d716dc93a67
roney ruiz <roneyruizrojas@gmail.com>
2024-09-05 10:29:10 -0600

    migrate the user module permissions

--- App/Infrastructure/Database/Migrations/20240905162315_FirstPermissionSeeding.Designer.cs
+++ App/Infrastructure/Database/Migrations/20240905162315_FirstPermissionSeeding.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pets_And_Paws_Api.App.Infrastructure.Database;
 
 #nullable disable
 
-namespace pets_and_paws_api.app.Database.Migrations
+namespace pets_and_paws_api.App.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240905162315_FirstPermissionSeeding")]
+    partial class FirstPermissionSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ App/Infrastructure/Database/Migrations/20240905162315_FirstPermissionSeeding.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace pets_and_paws_api.App.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstPermissionSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Permission",
+                columns: new[] { "Id", "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1278), "user:read", new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1280) },
+                    { 2, new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1283), "user:write", new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1283) },
+                    { 3, new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1284), "user:update", new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1285) },
+                    { 4, new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1286), "user:delete", new DateTime(2024, 9, 5, 16, 23, 15, 122, DateTimeKind.Utc).AddTicks(1286) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Permission",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
