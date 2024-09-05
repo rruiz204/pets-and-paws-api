e31ec7e40f401039897a20b936ecf4bb85d7d5a8
roney ruiz <roneyruizrojas@gmail.com>
2024-09-05 11:14:16 -0600

    fix typing

--- App/Domain/Models/Relationships/RoleScope.cs
+++ App/Domain/Models/Relationships/RoleScope.cs
@@ -3,8 +3,8 @@ namespace Pets_And_Paws_Api.App.Domain.Models.Relationships;
 public class RoleScope
 {
   public int RoleId  { get; set; }
-  public Role Role { get; set; }
+  public required Role Role { get; set; }
 
   public int PermissionId { get; set; }
-  public Scope Scope { get; set; }
+  public required Scope Scope { get; set; }
 }
