a6de184a775afd36b9e021eb1ed760c9d75d38ae
roney ruiz <roneyruizrojas@gmail.com>
2024-09-06 11:26:04 -0600

    implement startup file to config

--- Program.cs
+++ Program.cs
@@ -1,55 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using Pets_And_Paws_Api.App.Infrastructure.Configuration;
-using Pets_And_Paws_Api.App.Infrastructure.Database;
-using Pets_And_Paws_Api.App.Application.Services;
-using Pets_And_Paws_Api.App.Domain.Services;
-using Pets_And_Paws_Api.App.Infrastructure.Utilities;
-using Pets_And_Paws_Api.App.Domain.Utilities;
-using Pets_And_Paws_Api.App.Infrastructure.UnitOfWork;
+using Pets_And_Paws_Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Controllers
-builder.Services.AddControllers();
-
-// Database Context
-builder.Services.AddDbContext<DatabaseContext>(opts => 
-    opts.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
-
-// Services
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IRoleService, RoleService>();
-
-// Unit of Work
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-// Utilities
-builder.Services.AddScoped<IEncrypt, Encrypt>();
-builder.Services.AddScoped<ITokens, Tokens>();
-
-// Auto Mapper
-builder.Services.AddAutoMapper(typeof(Program));
-
-// Authentication & Authorization
-builder.Services.AddJwtAuthentication(builder.Configuration);
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
+startup.Configure(app, app.Environment);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-/* app.UseAuthentication();
-app.UseAuthorization(); */
-app.MapControllers();
 app.Run();
