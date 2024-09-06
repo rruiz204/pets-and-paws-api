59c2b351fd5092862644a14ebe122cb6b9718d47
roney ruiz <roneyruizrojas@gmail.com>
2024-09-06 17:32:51 -0600

    remove timestamp from role and scope models

--- App/Infrastructure/Database/Migrations/20240906233227_RemoveTimestampFromModels.Designer.cs
+++ App/Infrastructure/Database/Migrations/20240906233227_RemoveTimestampFromModels.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pets_And_Paws_Api.App.Infrastructure.Database;
@@ -11,9 +12,11 @@ using Pets_And_Paws_Api.App.Infrastructure.Database;
 namespace pets_and_paws_api.App.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240906233227_RemoveTimestampFromModels")]
+    partial class RemoveTimestampFromModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ App/Infrastructure/Database/Migrations/20240906233227_RemoveTimestampFromModels.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pets_and_paws_api.App.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTimestampFromModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Scopes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Scopes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Role");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Scopes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Scopes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Role",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Role",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
