46dcca797daf0339efdec91078014b66e9f8ec59
roney ruiz <roneyruizrojas@gmail.com>
2024-09-06 22:31:18 -0600

    run role and scope seeders

---
+++ App/Infrastructure/Database/Migrations/20240906235857_ApplyRoleAndScopeSeeders.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace pets_and_paws_api.App.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyRoleAndScopeSeeders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "Full access to all system functionalities", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Scopes",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Allows you to read user information", "user:read" },
+                    { 2, "Allows you to create new user records", "user:write" },
+                    { 3, "Allows modification of existing user information", "user:update" },
+                    { 4, "Allows deletion of user records", "user:delete" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Scopes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Scopes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Scopes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Scopes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
