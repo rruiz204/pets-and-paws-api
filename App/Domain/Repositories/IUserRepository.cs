8f1994a71da720ff024a56a9315768abb1f73280
roney ruiz <roneyruizrojas@gmail.com>
2024-09-09 17:25:41 -0600

    implement the new dtos to optimize queries in the repositories

--- App/Domain/Repositories/IUserRepository.cs
+++ App/Domain/Repositories/IUserRepository.cs
@@ -1,8 +1,10 @@
+using Pets_And_Paws_Api.App.Application.DTOs.Entities;
 using Pets_And_Paws_Api.App.Domain.Models;
 
 namespace Pets_And_Paws_Api.App.Domain.Repositories;
 
 public interface IUserRepository : IBaseRepository<User>
 {
+  Task<List<UserDTO>> GetAllUsersAsync();
   Task<User?> FindToValidRegister(User user);
 }
--- App/Infrastructure/Repositories/UserRepository.cs
+++ App/Infrastructure/Repositories/UserRepository.cs
@@ -2,14 +2,25 @@ using System.Linq.Expressions;
 using Pets_And_Paws_Api.App.Domain.Models;
 using Pets_And_Paws_Api.App.Infrastructure.Database;
 using Pets_And_Paws_Api.App.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Pets_And_Paws_Api.App.Application.DTOs.Entities;
 
 namespace Pets_And_Paws_Api.App.Infrastructure.Repositories;
 
 public class UserRepository(DatabaseContext context) : BaseRepository<User>(context), IUserRepository
-{
+{ 
   public async Task<User?> FindToValidRegister(User user)
   {
     Expression<Func<User, bool>> predicate = u => u.Email == user.Email || u.FirstName == user.FirstName;
     return await FindAsync(predicate);
+  }
+
+  public async Task<List<UserDTO>> GetAllUsersAsync()
+  {
+    return await dbSet.Select(user => new UserDTO {
+      Id = user.Id,
+      FullName = !string.IsNullOrEmpty(user.LastName) ? $"{user.FirstName} {user.LastName}" : user.FirstName,
+      Role = user.Role.Name
+    }).ToListAsync();
   }
 }
