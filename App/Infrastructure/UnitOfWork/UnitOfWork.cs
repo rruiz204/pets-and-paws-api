46fe923ffc5bdc270e4579b0c81906106f473378
roney ruiz <roneyruizrojas@gmail.com>
2024-09-10 14:23:39 -0600

    add mapper dependencie

--- App/Infrastructure/UnitOfWork/UnitOfWork.cs
+++ App/Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -1,15 +1,17 @@
+using AutoMapper;
 using Pets_And_Paws_Api.App.Domain.Repositories;
 using Pets_And_Paws_Api.App.Infrastructure.Database;
 using Pets_And_Paws_Api.App.Infrastructure.Repositories;
 
 namespace Pets_And_Paws_Api.App.Infrastructure.UnitOfWork;
 
-public class UnitOfWork(DatabaseContext context) : IUnitOfWork
+public class UnitOfWork(DatabaseContext context, IMapper mapper) : IUnitOfWork
 {
   private readonly DatabaseContext _context = context;
+  private readonly IMapper _mapper = mapper;
 
   private IUserRepository? _users;
-  public IUserRepository Users => _users ??= new UserRepository(_context);
+  public IUserRepository Users => _users ??= new UserRepository(_context, _mapper);
 
   private IResetTokenRepository? _tokens;
   public IResetTokenRepository Tokens => _tokens ??= new ResetTokenRepository(_context);
