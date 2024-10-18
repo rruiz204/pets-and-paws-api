b86b47a348ab349a3d3674e925cd4a5e3dc2e978
roney ruiz <roneyruizrojas@gmail.com>
2024-10-18 10:05:30 -0600

    registry password hasher

--- src/Application/Bootstrap.cs
+++ src/Application/Bootstrap.cs
@@ -1,5 +1,7 @@
 using Application.Services;
+using Domain.Entities;
 using Domain.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -9,6 +11,7 @@ public static class Bootstrap
   public static IServiceCollection AddApplication(this IServiceCollection services)
   {
     services.AddScoped<IHashPasswordService, HashPasswordService>();
+    services.AddScoped<PasswordHasher<User>>();
     return services;
   }
 }
