1257e66599d1f50b57691ba200350b4d1a384b87
roney ruiz <roneyruizrojas@gmail.com>
2024-10-18 23:52:28 -0600

    configure dependencies

--- src/Application/Bootstrap.cs
+++ src/Application/Bootstrap.cs
@@ -1,6 +1,10 @@
+using System.Reflection;
+using Application.Behaviours;
 using Application.Services;
 using Domain.Entities;
 using Domain.Services;
+using FluentValidation;
+using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +16,10 @@ public static class Bootstrap
   {
     services.AddScoped<IHashPasswordService, HashPasswordService>();
     services.AddScoped<PasswordHasher<User>>();
+
+    services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+    services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
     return services;
   }
 }
--- src/Infrastructure/Bootstrap.cs
+++ src/Infrastructure/Bootstrap.cs
@@ -3,6 +3,7 @@ using Infrastructure.Factories.DbContextFactory;
 using Infrastructure.Database.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Infrastructure.UnitOfWork;
 
 namespace Infrastructure;
 
@@ -11,7 +12,9 @@ public static class Bootstrap
   public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
   {
     AddDbContexts(services, configuration);
+
     services.AddTransient<IDbContextFactory, DbContextFactory>();
+    services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
     
     return services;
   }
