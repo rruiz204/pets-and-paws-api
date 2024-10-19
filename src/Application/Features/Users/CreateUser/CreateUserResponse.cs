8f37aedecc3c522d024b185fe7ed352c09f12b4c
roney ruiz <roneyruizrojas@gmail.com>
2024-10-18 23:57:01 -0600

    working on create user feature

---
+++ src/Application/Features/Users/CreateUser/CreateUserResponse.cs
@@ -0,0 +1,9 @@
+namespace Application.Features.Users.CreateUser;
+
+public class CreateUserResponse
+{
+  public int Id { get; set; }
+  public string FirstName { get; set; } = string.Empty;
+  public string LastName { get; set; } = string.Empty;
+  public string Email { get; set; } = string.Empty;
+}
