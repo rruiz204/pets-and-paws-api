6bcde73829706ec5d9e500f141a16e253757fa27
roney ruiz <roneyruizrojas@gmail.com>
2024-10-25 20:08:57 -0600

    delete all files

--- App/Domain/Exceptions/LogicException.cs
+++
@@ -1,6 +0,0 @@
-namespace Pets_And_Paws_Api.App.Domain.Exceptions;
-
-public class LogicException(string message, int code = 400) : Exception(message)
-{
-    public int StatusCode { get; } = code;
-}
--- App/Domain/Utilities/ITokens.cs
+++
@@ -1,8 +0,0 @@
-using Pets_And_Paws_Api.App.Domain.Models;
-
-namespace Pets_And_Paws_Api.App.Domain.Utilities;
-
-public interface ITokens
-{
-  string Generate(User user);
-}
