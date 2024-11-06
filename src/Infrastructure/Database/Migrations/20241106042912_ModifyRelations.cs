7dd721080fc16a5150c9d404fd414223b7e1deab
roney ruiz <roneyruizrojas@gmail.com>
2024-11-05 22:37:35 -0600

    feat: create modify relations migration

---
+++ src/Infrastructure/Database/Migrations/20241106042912_ModifyRelations.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_Role_RolesId",
+                table: "UserRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_User_UsersId",
+                table: "UserRole");
+
+            migrationBuilder.DropTable(
+                name: "RoleClaim");
+
+            migrationBuilder.DropTable(
+                name: "Claim");
+
+            migrationBuilder.RenameColumn(
+                name: "UsersId",
+                table: "UserRole",
+                newName: "RoleId");
+
+            migrationBuilder.RenameColumn(
+                name: "RolesId",
+                table: "UserRole",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRole_UsersId",
+                table: "UserRole",
+                newName: "IX_UserRole_RoleId");
+
+            migrationBuilder.CreateTable(
+                name: "Scope",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scope", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleScope",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(type: "integer", nullable: false),
+                    ScopeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleScope", x => new { x.RoleId, x.ScopeId });
+                    table.ForeignKey(
+                        name: "FK_RoleScope_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleScope_Scope_ScopeId",
+                        column: x => x.ScopeId,
+                        principalTable: "Scope",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleScope_ScopeId",
+                table: "RoleScope",
+                column: "ScopeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                table: "UserRole",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_User_UserId",
+                table: "UserRole",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                table: "UserRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_User_UserId",
+                table: "UserRole");
+
+            migrationBuilder.DropTable(
+                name: "RoleScope");
+
+            migrationBuilder.DropTable(
+                name: "Scope");
+
+            migrationBuilder.RenameColumn(
+                name: "RoleId",
+                table: "UserRole",
+                newName: "UsersId");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "UserRole",
+                newName: "RolesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRole_RoleId",
+                table: "UserRole",
+                newName: "IX_UserRole_UsersId");
+
+            migrationBuilder.CreateTable(
+                name: "Claim",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Claim", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleClaim",
+                columns: table => new
+                {
+                    ClaimsId = table.Column<int>(type: "integer", nullable: false),
+                    RolesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleClaim", x => new { x.ClaimsId, x.RolesId });
+                    table.ForeignKey(
+                        name: "FK_RoleClaim_Claim_ClaimsId",
+                        column: x => x.ClaimsId,
+                        principalTable: "Claim",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleClaim_Role_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleClaim_RolesId",
+                table: "RoleClaim",
+                column: "RolesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_Role_RolesId",
+                table: "UserRole",
+                column: "RolesId",
+                principalTable: "Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_User_UsersId",
+                table: "UserRole",
+                column: "UsersId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
